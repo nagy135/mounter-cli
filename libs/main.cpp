20e82136573df90c509554afe7c3ecc13ed89e9b
nagy135 <viktor.nagy1995@gmail.com>
2018-09-11 13:10:14 +0200

    credentials check

--- libs/main.cpp
+++ libs/main.cpp
@@ -1,40 +1,43 @@
 #include <ncurses.h>
 #include <string.h>
 #include "mounter.h"
+#include <unistd.h> // getuid
+#include <stdio.h> // printf
 
 
-int main()
-{
- char title[]="mounter-cli v1.0";
- char mesg[]="press any key to continue (or q to exit)";
- int row,col;
- initscr();
- noecho();
- curs_set(0);
- getmaxyx(stdscr,row,col);
- mvprintw(row/2,(col-strlen(title))/2,"%s",title);
- mvprintw(row/2+2,(col-strlen(mesg))/2,"%s",mesg);
-                                	/* print the message at the center of the screen */
- mvprintw(row-2,0,"C++ mounter\n");
- printw("CLI, ncurses based device mounter");
+int main(){
+    if (getuid()){
+        printf("%s", "You must be root to mount devices!\n");
+        return -1;
+    }
 
- char quit = 'q';
- int pressed = getch();
- if (pressed == (int)quit){
-     endwin();
-     return 0;
- }
- else{
+    char title[]="mounter-cli v1.0";
+    char mesg[]="press any key to continue (or q to exit)";
+    int row,col;
+    initscr();
+    noecho();
+    curs_set(0);
+    getmaxyx(stdscr,row,col);
+    mvprintw(row/2,(col-strlen(title))/2,"%s",title);
+    mvprintw(row/2+2,(col-strlen(mesg))/2,"%s",mesg);
+                                    /* print the message at the center of the screen */
+    mvprintw(row-2,0,"C++ mounter\n");
+    printw("CLI, ncurses based device mounter");
 
-     clear();
-     refresh();
-     Mounter mntr(col, row);
-     /* Note a; */
-     /* printw("Presse %c",(char) pressed); */
- }
+    char quit = 'q';
+    int pressed = getch();
+    if (pressed == (int)quit){
+        endwin();
+        return 0;
+    } else {
+        clear();
+        refresh();
+        Mounter mntr(col, row);
+        /* Note a; */
+        /* printw("Presse %c",(char) pressed); */
+    }
+    refresh();
+    endwin();
 
- refresh();
- endwin();
-
- return 0;
+    return 0;
 }
